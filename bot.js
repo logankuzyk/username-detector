cbd8a60de4d6885dd7cc9bcad0e6f723a71dd462
Logan Kuzyk <logan@kuzyk.net>
2020-02-22 16:18:28 -0800

    Get latest post from subreddit, process it

--- bot.js
+++ bot.js
@@ -1,29 +1,57 @@
-const Reddit = require('reddit')
-const Tesseract = require('tesseract.js')
 const dotenv = require('dotenv').config()
+const Reddit = require('snoowrap')
+const Tesseract = require('tesseract.js')
 
-class Bot {
-    constructor() {
-        this.reddit = new Reddit({
-            username: process.env.USERNAME,
-            password: process.env.PASSWORD,
-            appId: process.env.CLIENT_ID,
-            appSecret: process.env.CLIENT_SECRET,
-            userAgent: process.env.USERAGENT
-        })
-        this.ocr = Tesseract
-        this.time = new Date()
-    }
+// let post = {}
+let time = new Date ()
+let start = time.getTime()
 
-    listen() {
+const reddit = new Reddit ({
+    userAgent: process.env.USERAGENT,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+})
 
-    }
+// async function moderate (submission) {
+//     this.reddit.post('/api/comment', {
+//         api_type: 'json',
+//         text: 'Your image seems to contain a reddit username. Please read the rules of the subreddit, censor your image, and try again.'
+        
+//     })
+// }
 
-    process() {
+async function scan (submission) {
+    let image = await submission.url
+    await Tesseract.recognize(
+        image,
+        'eng',
+    { logger: m => console.log(m) }
+    ).then(({ data: { text } }) => {
+        if (text.includes('u/')) {
+            // moderate(submission);
+            console.log("USERNAME FOUND")
+        } else {
+            console.log("NO USERNAME")
+        }
+    })
+}
 
-    }
-
-    comment() {
-
+async function parse (submission) {
+    if (submission.url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+        scan(submission)
+    } else {
+        console.log('not an image')
+        return
     }
 }
+
+async function update () {
+    post = await reddit.getSubreddit('dankmemes').getNew({limit: 1})[0]
+    parse(post)
+}
+
+update()
+
+console.log(time.getTime() - start)
